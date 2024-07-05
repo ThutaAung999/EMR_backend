bd8f6ddb7dfa67dacef33363d330cb17b06a1f99
ThutaAung999 <exercise999123@gmail.com>
2024-07-05 23:11:38 +0630

    a little update

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,18 @@
 
-import  dovenv from 'dotenv'
+import  dotenv from 'dotenv'
+dotenv.config();
 
 interface Config {
     db: string;
 }
 
 const config: Config = {
-    /*db: 'mongodb://0.0.0.0:27017/electrical_medicine_record'*/
-    //    db: 'mongodb+srv://exercise999123:htein@cluster0.nx2m3ak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    //db: 'mongodb://0.0.0.0:27017/electrical_medicine_record'
+    //db: 'mongodb+srv://exercise999123:htein@cluster0.nx2m3ak.mongodb.net/Electrical_Medicine_Record'
+
     db : process.env.DATABASE as string,
 };
 
-
-
 export default config;
+
+
--- src/config/env.ts
+++ src/config/env.ts
@@ -15,8 +15,6 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
 */
 
-
-
 export {
   //PORT,
   NODE_ENV,
