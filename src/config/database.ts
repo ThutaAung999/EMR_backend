38196dae1a235f412d43ae3c08e0cec3ae7c1545
ThutaAung999 <exercise999123@gmail.com>
2024-12-27 09:17:21 +0630

    a little update

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,8 +9,8 @@ const config: Config = {
   //db: 'mongodb://0.0.0.0:27017/electrical_medicine_record'
   //db: 'mongodb+srv://exercise999123:htein@cluster0.nx2m3ak.mongodb.net/Electrical_Medicine_Record'
 
-  //db : process.env.DATABASE as string,
-  db: process.env.DATABASE_LOCAL as string,
+  db : process.env.DATABASE as string,
+  //db: process.env.DATABASE_LOCAL as string,
 };
 
 export default config;
